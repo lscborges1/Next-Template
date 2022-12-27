a26b0b8b600234b13fd76f8156855289598bc603
Lucas Borges <lscborges1@gmail.com>
2022-12-27 02:03:09 -0300

    fix: remove babel config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const isProd = process.env.NODE_ENV === 'production'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: !isProd
+  disable: !isProd,
+  swcMinify: true,
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  }
 })
 
 module.exports = withPWA()
